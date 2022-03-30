aab742e21663ebc442918020d128b7733209eac3
GHMan2021 <statio2009@gmail.com>
2022-03-31 02:18:28 +0400

    modify test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,11 +9,9 @@ def test_generate_diff():
     file1 = Path(Path.cwd() / 'tests' / 'fixtures' / 'file1.json')
     file2 = Path(Path.cwd() / 'tests' / 'fixtures' / 'file2.json')
     diff = generate_diff(file1, file2)
+    result_json = open(Path(Path.cwd() / 'tests' / 'fixtures' / 'result_json'))
 
-    assert diff == \
-           '{\n- follow: false\n  host: hexlet.io\n' \
-           '- proxy: 123.234.53.22\n- timeout: 50\n' \
-           '+ timeout: 20\n+ verbose: true\n}'
+    assert diff == result_json.read()
 
 
 def test_type_output():
