b616bca8dfe855149211ecc04e6a4666b49b587f
GHMan2021 <statio2009@gmail.com>
2022-04-02 01:49:05 +0400

    modify generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,22 +4,23 @@ from pathlib import Path
 from gendiff.replace_bool import replace_bool
 
 
-def generate_diff(file1, file2):
-    path_file1 = Path(Path.cwd() / file1)
-    path_file2 = Path(Path.cwd() / file2)
-    path_files = [path_file1, path_file2]
+def get_list_files(file1, file2):
+    suffix_files = list(map(lambda x: Path(Path(x).name).suffix, (file1, file2)))
 
-    suffix_file1 = Path(Path(file1).name).suffix
-    suffix_file2 = Path(Path(file2).name).suffix
-    suffix_files = [suffix_file1, suffix_file2]
+    path_files = list(map(lambda x: Path(Path.cwd() / x), (file1, file2)))
 
     list_files = []
-    for i in range(2):
-        if suffix_files[i] == '.json':
+    i = 0
+    for _ in suffix_files:
+        if _ not in ['.json', 'yml', 'yaml']:
+            return "File format is wrong"
+
+        if _ == '.json':
             file_json = json.load(open(path_files[i]))
             file_json = replace_bool(file_json)
             list_files.append(file_json)
-        elif suffix_files[i] in ['.yml', '.yaml']:
+            i = 1
+        else:
             stream = open(path_files[i])
             file_yml = yaml.safe_load(stream)
             dict_files = {}
@@ -27,15 +28,17 @@ def generate_diff(file1, file2):
                 dict_files.update(j)
             dict_files = replace_bool(dict_files)
             list_files.append(dict_files)
-        else:
-            return "File format is wrong"
-    file_1 = list_files[0]
-    file_2 = list_files[1]
-    # сформировать уникальные значения в 2-х файлах, сортировка по алфавиту
-    list_keys_1 = list(file_1.keys())
-    list_keys_2 = list(file_2.keys())
-    list_keys = set(list_keys_1 + list_keys_2)
-    list_keys = sorted(list_keys)
+            i = 1
+    return list_files
+
+
+def generate_diff(file1, file2):
+    list_files = get_list_files(file1, file2)
+
+    file_1, file_2 = list_files[0], list_files[1]
+    list_keys_1, list_keys_2 = list(file_1.keys()), list(file_2.keys())
+
+    list_keys = sorted(set(list_keys_1 + list_keys_2))
 
     result = []
     for key in list_keys:
