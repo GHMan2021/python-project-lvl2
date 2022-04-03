522043182ee1787ab71bbae7799a34aa064f9722
GHMan2021 <statio2009@gmail.com>
2022-04-03 14:02:27 +0400

    modify generate_diff.py

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ test:
 	poetry run pytest tests/ -v
 test-coverage:
 	poetry run pytest --cov=gendiff --cov-report=xml tests/
+# временные команды для проведения тестов
+t:
+	python -m pytest --cov=gendiff -v
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,51 +1,46 @@
 import json
 import yaml
+from functools import wraps
 from pathlib import Path
 from gendiff.replace_bool import replace_bool
 
 
-def get_list_files(file1, file2):
-    # список расширений файлов
-    suffix_files = \
-        list(map(lambda x: Path(Path(x).name).suffix, (file1, file2)))
+def path_and_suffix(func):
+    @wraps(func)
+    def wrapper(file):
+        path_file = Path(Path.cwd() / file)
+        suffix_file = Path(Path(file).name).suffix
+        data_file_tuple = (path_file, suffix_file)
+        return func(data_file_tuple)
+    return wrapper
 
-    # список путей файлов и проверка их правильности
-    path_files = list(map(lambda x: Path(Path.cwd() / x), (file1, file2)))
-    path_files_exists = list(filter(lambda x: Path(x).exists(), path_files))
-    if len(path_files_exists) != 2:
+
+@path_and_suffix
+def is_correct_file(file):
+    if not file[0].exists():
         return "File path is wrong"
 
-    # формирование списка словарей из 2х переданных файлов
-    list_files = []
-    i = 0
-    for suffix in suffix_files:
-        if suffix not in ['.json', '.yml', '.yaml']:
-            return "File format is wrong"
-
-        if suffix == '.json':
-            file_json = json.load(open(path_files[i]))
-            file_json = replace_bool(file_json)
-            list_files.append(file_json)
-            i = 1
-        else:
-            stream = open(path_files[i])
-            file_yml = yaml.safe_load(stream)
-            dict_files = {}
-            for j in file_yml:
-                dict_files.update(j)
-            dict_files = replace_bool(dict_files)
-            list_files.append(dict_files)
-            i = 1
-    return list_files
+    if file[1] not in ['.json', '.yml', '.yaml']:
+        return "File format is wrong"
 
 
-def generate_diff(file1, file2):
-    # сформировать список из 2 словарей из файлов с проверкой корректности
-    list_files = get_list_files(file1, file2)
-    if isinstance(list_files, str):
-        return list_files
+@path_and_suffix
+def prepare_dict(file):
+    if file[1] == '.json':
+        file_json = json.load(open(file[0]))
+        file_json = replace_bool(file_json)
+        return file_json
+    stream = open(file[0])
+    file_yml = yaml.safe_load(stream)
+    dict_files = {}
+    for j in file_yml:
+        dict_files.update(j)
+    dict_files = replace_bool(dict_files)
+    return dict_files
 
-    file_1, file_2 = list_files[0], list_files[1]
+
+def diff(list_dicts):
+    file_1, file_2 = list_dicts[0], list_dicts[1]
     list_keys_1, list_keys_2 = list(file_1.keys()), list(file_2.keys())
 
     list_keys = sorted(set(list_keys_1 + list_keys_2))
@@ -67,10 +62,30 @@ def generate_diff(file1, file2):
                 line_2 = ("+ " + str(key) + ": " + str(file_2[key]))
                 result.append(line_1)
                 result.append(line_2)
+    return result
 
-    # вывод результата по шаблону
+
+def output(result):
     prn = "{\n"
     for i in result:
         prn += i + '\n'
     prn = prn + '}'
     return prn
+
+
+def generate_diff(file1, file2):
+    list_dicts = []
+
+    # проверка пути и формата файла, вывод соответствующего сообщения при ошибки
+    for file in (file1, file2):
+        if isinstance(is_correct_file(file), str):
+            return is_correct_file(file)
+        # функция подготовки содержимого файла в словарь
+        dict_file = prepare_dict(file)
+        list_dicts.append(dict_file)
+
+    # функция сравнения словарей
+    result = diff(list_dicts)
+
+    # функция вывода результата в заданном формате
+    return output(result)
